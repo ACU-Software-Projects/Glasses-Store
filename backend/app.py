56b8b5270fd83948b173d4520e759f5e3dfa606a
Mostafa Osman <130773676+MostafaOsmanFathi@users.noreply.github.com>
2024-11-26 16:49:42 +0200

    cart api and cart checkout api

--- backend/app.py
+++ backend/app.py
@@ -124,6 +124,8 @@ def add_product():
 
 
 app.route('/product/', methods=['GET'])
+
+
 def get_products():
     # TODO Retrieve all products from database in data var
     data = ""
@@ -131,6 +133,8 @@ def get_products():
 
 
 app.route('/product/<int:product_id>', methods=['GET'])
+
+
 def get_product(product_id):
     # TODO check product in database by id and get it's all data
     # if product_id not in ##DATABASE##:
@@ -138,7 +142,10 @@ def get_product(product_id):
     productInfo = ""
     return jsonify(productInfo), 200
 
+
 app.route('/product/<int:product_id>', methods=['DELETE'])
+
+
 def delete_product(product_id):
     try:
         data = request.get_json()
@@ -156,12 +163,13 @@ def delete_product(product_id):
         if logged_in_session[api_key]['account_type'] != 'ADMIN':
             return jsonify({"error": f"API key {api_key} not Admin."}), 401
 
-        #TODO send request to database to delete product using product_id and check if product exisit
+        # TODO send request to database to delete product using product_id and check if product exisit
 
     except Exception as e:
         return jsonify({"error": "An unexpected error occurred.", "details": str(e)}), 500
 
-@app.route('payment/',methods=['POST'])
+
+@app.route('payment/', methods=['POST'])
 def deposit():
     try:
         data = request.get_json()
@@ -169,22 +177,25 @@ def deposit():
             return jsonify({"error": "Invalid input. No data provided."}), 400
         required_fields = ['api_key', 'amount']
 
-        api_key= data['api_key']
+        api_key = data['api_key']
         amount = data['amount']
 
-        #TODO make deposit in database
+        # TODO make deposit in database
         return jsonify({"message": "Deposit successful!"}), 201
     except Exception as e:
         return jsonify({"error": "An unexpected error occurred.", "details": str(e)}), 500
 
-@app.route('/payment/withdraw',methods=['POST'])
+
+@app.route('/payment/withdraw', methods=['POST'])
 def withdraw():
     try:
         data = request.get_json()
         if not data:
             return jsonify({"error": "Invalid input. No data provided."}), 400
         required_fields = ['api_key', 'amount']
-
+        missing_fields = [field for field in required_fields if field not in data]
+        if missing_fields:
+            return jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}), 400
         api_key = data['api_key']
         amount = data['amount']
 
@@ -195,5 +206,43 @@ def withdraw():
         return jsonify({"error": "An unexpected error occurred.", "details": str(e)}), 500
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('cart/add_product', methods=['POST'])
+def add_cart():
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Invalid input. No data provided."}), 400
+        required_fields = ['api_key', 'product_id']
+        missing_fields = [field for field in required_fields if field not in data]
+        if missing_fields:
+            return jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}), 400
+        # TODO add product to cart database
+    except Exception as e:
+        return jsonify({"error": "An unexpected error occurred.", "details": str(e)}), 500
+
+
+# TODO make function that create Invoice
+@app.route('/cart/checkout', methods=['POST'])
+def checkout():
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Invalid input. No data provided."}), 400
+        required_fields = ['api_key', 'cart_id']
+        missing_fields = [field for field in required_fields if field not in data]
+        if missing_fields:
+            return jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}), 400
+        api_key = data['api_key']
+
+        if api_key not in logged_in_session:
+            return jsonify({"error": f"API key {api_key} not found."}), 401
+
+        # TODO make invoice in the data base
+        # get it's data
+        # return it's data to the frontend
+
+    except Exception as e:
+        return jsonify({"error": "An unexpected error occurred.", "details": str(e)}), 500
+
+    if __name__ == '__main__':
+        app.run()
